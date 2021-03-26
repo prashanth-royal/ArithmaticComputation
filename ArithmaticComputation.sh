9ce1e1240f5506b6377ddf8a20525a1720d9b283
Prashanth <prashanth.royal2046@gmail.com>
2021-03-26 12:18:13 +0530

    Added: values stored in array

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -8,4 +8,5 @@ val[1]=$(($a+$b*$c))
 val[2]=$(($a*$b+$c))
 val[3]=$(($c+$a/$b))
 val[4]=$(($a%$b+$c))
-	echo "All Arithmatic Values :: " ${val[*]}
+	echo "All Arithmatic Values :: " ${val[@]}
+	echo ${val[@]}
