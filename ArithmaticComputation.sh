9acbe41dbf8879032b6baef569663ed4fc756940
Prashanth <prashanth.royal2046@gmail.com>
2021-03-26 12:10:10 +0530

    Added: values stored in dictionary

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -3,11 +3,9 @@
 echo "Enter the three inputs :: "
 read a b c
 
-val1=$(($a+$b*$c))
-val2=$(($a*$b+$c))
-val3=$(($c+$a/$b))
-val4=$(($a%$b+$c))
-	echo $val1
-	echo $val2
-	echo $val3
-	echo $val4
+declare -A val
+val[1]=$(($a+$b*$c))
+val[2]=$(($a*$b+$c))
+val[3]=$(($c+$a/$b))
+val[4]=$(($a%$b+$c))
+	echo "All Arithmatic Values :: " ${val[*]}
