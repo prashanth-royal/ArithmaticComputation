85bb7fa501de3dfc3ab60d103e117d1690e53884
Prashanth <prashanth.royal2046@gmail.com>
2021-03-30 09:20:12 +0530

    Added: Values in descending order

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -10,3 +10,17 @@ val[3]=$(($c+$a/$b))
 val[4]=$(($a%$b+$c))
 	echo "All Arithmatic Values :: " ${val[@]}
 	echo ${val[@]}
+
+
+echo "Sorting Values from Descending Order"
+
+echo "Enter value 1"
+read val[1]
+echo "Enter value 2"
+read val[2]
+echo "Enter value 3"
+read val[3]
+echo "Enter value 4"
+read val[4]
+
+	echo Descending order is $(printf "%s\n" ${val[@]} | sort -nr)
